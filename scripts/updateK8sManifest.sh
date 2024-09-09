9af841e9b6070eac44ade613f1c09b819e67ae51
indatech gaming <indatechgaming@gmail.com>
2024-09-09 09:30:18 +0000

    Updated updateK8sManifest.sh

--- scripts/updateK8sManifest.sh
+++ scripts/updateK8sManifest.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Set the repository URL
-REPO_URL="https://dev.azure.com/indatechgaming/go-web-app"
+REPO_URL="https://duqswwdc4tnmn2aroohas76y5unnnchl53r2xzilri3yn56teqmq@dev.azure.com/indatechgaming/go-web-app/_git/go-web-app"
 
 # Clone the git repository into the /tmp directory
 git clone "$REPO_URL" /tmp/temp_repo
